3b0dc6df295af475ffe364c7d944fc8b5fec5170
solutionpanacea <dk779951@gmail.com>
2025-08-24 20:02:33 +0530

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// firebaseConfig.js
+// Your Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyD6hPxdAYnaT92xoqfKOnGZhV1AbzBHwyI",
+  authDomain: "solution-a0595.firebaseapp.com",
+  projectId: "solution-a0595",
+  storageBucket: "solution-a0595.appspot.com",
+  messagingSenderId: "38814647745",
+  appId: "1:38814647745:web:01e1f57baf2ae2a5265b40"
+};
